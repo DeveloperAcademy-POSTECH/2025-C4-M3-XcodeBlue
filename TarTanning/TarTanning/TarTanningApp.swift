3c85e44a9034caf0e8bce9783126a236740cf5cb
J <knowisdom314@gmail.com>
2025-07-11 11:40:24 +0900

    chore: 프로젝트 파일 설정 (SwiftLint, gitignore)

---
+++ TarTanning/TarTanning/TarTanningApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarTanningApp.swift
+//  TarTanning
+//
+//  Created by J on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TarTanningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TarTanning/TarTanningWatch Watch App/TarTanningWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  TarTanningWatchApp.swift
+//  TarTanningWatch Watch App
+//
+//  Created by J on 7/11/25.
+//
+
+import SwiftUI
+
+@main
+struct TarTanningWatchWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

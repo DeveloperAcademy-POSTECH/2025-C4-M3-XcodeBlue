27f73661c43dc4752927f4a1204c57e859c5b31a
jun <wnsgur4092@gmail.com>
2025-07-17 16:50:10 +0900

    feat: 선크림 SPF 지수 초기값 설정

--- TarTanning/TarTanning/Data/Models/User/UserProfile.swift
+++ TarTanning/TarTanning/Data/Models/User/UserProfile.swift
@@ -12,11 +12,12 @@ import SwiftData
 @Model
 class UserProfile {
     var skinType: SkinType
-    var currentSpfIndex: Int?
+    var currentSpfIndex: Int = 30 // 사용자가 설정한 SPF (기본값 30)
     var spfDate: Date? // SPF 발라진 날짜/시간
     var dailyExposures: [DailyUVExpose] = []
     
     init(skinType: SkinType) {
         self.skinType = skinType
+        self.currentSpfIndex = 30 // 기본값 설정
     }
 }

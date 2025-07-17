848657b376beb1c3ed2367b8437c4d2301ce66a2
jun <wnsgur4092@gmail.com>
2025-07-17 15:19:09 +0900

    feat: 선크림 모드 데이터 모델 구현

--- TarTanning/TarTanning/Data/Models/Sunscreen/SunScreenInfo.swift
+++ TarTanning/TarTanning/Data/Models/Sunscreen/SunScreenInfo.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+struct SunScreenInfo {
+    let spfIndex: Int
+    let activationTime: Date
+    
+    static let duration: TimeInterval = 2 * 60 * 60 // 2시간 (초 단위)
+    static let defaultSPFIndex = 30
+}

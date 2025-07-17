381fa9beb05d93ab363aa7a27e2ba472ee615e36
jun <wnsgur4092@gmail.com>
2025-07-17 12:32:28 +0900

    feat: 날짜별 사용자 전체 활동(일광시간,UV노출량)을 담는 데이터 구조체 구현

---
+++ TarTanning/TarTanning/Data/Models/DailyUVExpose.swift
@@ -0,0 +1,21 @@
+//
+//  DailyUVExposure.swift
+//  TarTanning
+//
+//  Created by Jun on 7/14/25.
+//
+
+import Foundation
+
+/// 날짜별 사용자의 전체 일광시간, UV노출량 기록을 담는 데이터 구조
+struct DailyUVExpose {
+    let date: Date
+    var exposureRecords: [UVExposeRecord] = []
+    var totalUVDose: Double {
+        exposureRecords.reduce(0) {$0 + $1.uvDose}
+    }
+    var totalSunlightMintues: Double{
+        let totalMinutes = exposureRecords.reduce(0) {$0 + $1.sunlightExposureDuration}
+        return totalMinutes
+    }
+}
--- TarTanning/TarTanning/Data/Models/UVExposeRecord.swift
+++ TarTanning/TarTanning/Data/Models/UVExposeRecord.swift
@@ -7,13 +7,19 @@
 
 import Foundation
 
-struct UVExposeRecord {
-    var startDate: Date // 일광 노출이 시작된 시간 (11:42)
-    var endDate: Date // 일광 노출이 시작된 시간 (12:15)
-    var sunlightExposureDuration: Double // startDate ~ endDate 사이의 일광 시간
-    var uvDose: Double // start Date ~ endDate 시간 사이의 UV 누적량 (UV노출량 식 계산, 단위 J/m^2)
-    
-    init(startDate: Date, endDate: Date, sunlightExposureDuration: Double, uvDose: Double) {
+struct UVExposeRecord: Identifiable {
+    let id = UUID()
+    var startDate: Date  // 일광 노출이 시작된 시간 (11:42)
+    var endDate: Date  // 일광 노출이 시작된 시간 (12:15)
+    var sunlightExposureDuration: Double  // startDate ~ endDate 사이의 일광 시간
+    var uvDose: Double  // start Date ~ endDate 시간 사이의 UV 누적량 (UV노출량 식 계산, 단위 J/m^2)
+
+    init(
+        startDate: Date,
+        endDate: Date,
+        sunlightExposureDuration: Double,
+        uvDose: Double
+    ) {
         self.startDate = startDate
         self.endDate = endDate
         self.sunlightExposureDuration = sunlightExposureDuration

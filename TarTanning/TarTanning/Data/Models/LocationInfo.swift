70360ebddb94b43ea1b08343e6acc6a4f7fb7458
jun <wnsgur4092@gmail.com>
2025-07-17 11:39:30 +0900

    feat: 유저 정보를 담는 구조체 구현

--- TarTanning/TarTanning/Data/Models/LocationInfo.swift
+++ TarTanning/TarTanning/Data/Models/LocationInfo.swift
@@ -5,4 +5,14 @@
 //  Created by Jun on 7/14/25.
 //
 
+import CoreLocation
 import Foundation
+
+struct LocationInfo {
+    let latitude: Double
+    let longitude: Double
+    let city: String  // 포항시
+    var asCLLocation: CLLocation {
+        CLLocation(latitude: latitude, longitude: longitude)
+    }
+}

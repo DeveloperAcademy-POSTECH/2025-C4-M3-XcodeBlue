69b6f887808eb3fe7a41279ba2b2eb78bebefe5b
jun <wnsgur4092@gmail.com>
2025-07-18 17:58:35 +0900

    feat: UserProfile 데이터 모델 UserDefault로 변경

--- TarTanning/TarTanning/Data/Models/User/UserProfile.swift
+++ TarTanning/TarTanning/Data/Models/User/UserProfile.swift
@@ -6,18 +6,8 @@
 //
 
 import Foundation
-import SwiftUI
-import SwiftData
 
-@Model
-class UserProfile {
-    var skinType: SkinType
-    var currentSpfIndex: Int = 30 // 사용자가 설정한 SPF (기본값 30)
-    var spfDate: Date? // SPF 발라진 날짜/시간
-    var dailyExposures: [DailyUVExpose] = []
-    
-    init(skinType: SkinType) {
-        self.skinType = skinType
-        self.currentSpfIndex = 30 // 기본값 설정
-    }
+struct UserProfile: Codable {
+    var skinType: SkinType // 유저의 스킨 유형: .type3
+    var currentSPFIndex: Double // 현재 선택된 SPF 지수: 30
 }

8475a2e138fbb9bfc3edb4f7a5183f11cd9afb2d
jun <wnsgur4092@gmail.com>
2025-07-18 20:42:45 +0900

    feat: UserProfile MockData 추가

--- TarTanning/TarTanning/Data/Models/User/UserProfile.swift
+++ TarTanning/TarTanning/Data/Models/User/UserProfile.swift
@@ -10,3 +10,7 @@ import Foundation
 struct UserProfile: Codable {
     var skinType: SkinType // 유저의 스킨 유형: .type3
 }
+
+extension UserProfile {
+    static let mockUser = UserProfile(skinType: .type3)
+}

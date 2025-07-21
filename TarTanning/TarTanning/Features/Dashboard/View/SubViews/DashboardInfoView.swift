e9cb808eefa9adeb5c7070ce8152a25ac665691b
llj72 <85468841+llj72@users.noreply.github.com>
2025-07-22 01:49:36 +0900

    feature: dashboard info(article)뷰 구현

---
+++ TarTanning/TarTanning/Features/Dashboard/View/SubViews/DashboardInfoView.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardInfoView.swift
+//  TarTanning
+//
+//  Created by 강진 on 7/21/25.
+//
+
+import SwiftUI
+
+struct DashboardInfoView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DashboardInfoView()
+}
---
+++ TarTanning/TarTanning/Features/Dashboard/ViewModel/DashboardInfoViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DashboardInfoViewModel.swift
+//  TarTanning
+//
+//  Created by 강진 on 7/21/25.
+//
+
+import Foundation

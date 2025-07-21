c4f2a8d0eb08436e9eee9d0cb1f3b3b6745aa75b
jun <wnsgur4092@gmail.com>
2025-07-21 16:22:13 +0900

    chore: App 진입점 변경

--- TarTanning/TarTanning/TarTanningApp.swift
+++ TarTanning/TarTanning/TarTanningApp.swift
@@ -11,8 +11,7 @@ import SwiftUI
 struct TarTanningApp: App {
     var body: some Scene {
         WindowGroup {
-//            RootView()
-            ContentView()
+            RootView()
         }
     }
 }

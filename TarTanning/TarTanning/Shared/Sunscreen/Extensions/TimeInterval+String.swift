8507677bc2d6ef82e94623f4a13c4e92f0b503a9
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-07-22 17:35:19 +0900

    fix: 타겟 풀림 수정

---
+++ TarTanning/TarTanning/Shared/Sunscreen/Extensions/TimeInterval+String.swift
@@ -0,0 +1,39 @@
+//
+//  TimeInterval.swift
+//  TarTanning
+//
+//  Created by taeni on 7/19/25.
+//
+
+import Foundation
+
+extension TimeInterval {
+    var timeDisplayString: String {
+        let hours = Int(self) / 3600
+        let minutes = Int(self) % 3600 / 60
+        let seconds = Int(self) % 60
+        
+        if hours > 0 {
+            return String(format: "%d:%02d:%02d", hours, minutes, seconds)
+        } else {
+            return String(format: "%02d:%02d", minutes, seconds)
+        }
+    }
+    
+    var shortTimeString: String {
+        let minutes = Int(self) / 60
+        let seconds = Int(self) % 60
+        return String(format: "%d:%02d", minutes, seconds)
+    }
+    
+    var hoursAndMinutes: String {
+        let hours = Int(self) / 3600
+        let minutes = Int(self) % 3600 / 60
+        
+        if hours > 0 {
+            return String(format: "%d시간 %d분", hours, minutes)
+        } else {
+            return String(format: "%d분", minutes)
+        }
+    }
+}

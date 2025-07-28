2c72613d73f305c9bca7480426f2d69a0d649475
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-07-28 11:58:20 +0900

    feat: watchOS - iOS 타이머 연동

--- TarTanning/TarTanning/Shared/Sunscreen/Extensions/TimeInterval+String.swift
+++ TarTanning/TarTanning/Shared/Sunscreen/Extensions/TimeInterval+String.swift
@@ -11,12 +11,11 @@ extension TimeInterval {
     var timeDisplayString: String {
         let hours = Int(self) / 3600
         let minutes = Int(self) % 3600 / 60
-        let seconds = Int(self) % 60
         
         if hours > 0 {
-            return String(format: "%d:%02d:%02d", hours, minutes, seconds)
+            return String(format: "%02d:%02d", hours, minutes)
         } else {
-            return String(format: "%02d:%02d", minutes, seconds)
+            return String(format: "00:%02d", minutes)
         }
     }
     

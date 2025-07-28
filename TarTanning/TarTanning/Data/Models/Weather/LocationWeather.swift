b2684e78905852b8dd8cfe407684e3d88c8d633b
TAENI <52228832+delightPIP@users.noreply.github.com>
2025-07-28 17:40:45 +0900

    fix: 현재 시간 UVIndex 추출

--- TarTanning/TarTanning/Data/Models/Weather/LocationWeather.swift
+++ TarTanning/TarTanning/Data/Models/Weather/LocationWeather.swift
@@ -32,3 +32,16 @@ final class LocationWeather {
         self.hourlyWeathers = hourlyWeathers
     }
 }
+
+extension LocationWeather {
+    func currentUVIndex(at date: Date = Date()) -> Double? {
+        // 현재 시간의 시간(Hour)만 추출
+        let currentHour = Calendar.current.component(.hour, from: date)
+        
+        // hourlyWeathers 중 현재 시간과 일치하는 hour를 찾음
+        // 또는 가장 가까운 시간대의 UV index를 가져올 수도 있음
+        let closestWeather = hourlyWeathers.min(by: { abs($0.hour - currentHour) < abs($1.hour - currentHour) })
+
+        return closestWeather?.uvIndex
+    }
+}
---
+++ TarTanning/TarTanning/Shared/Sunscreen/Models/Enums/UVLevel.swift
@@ -0,0 +1,8 @@
+//
+//  UVLevel.swift
+//  TarTanning
+//
+//  Created by taeni on 7/28/25.
+//
+
+import Foundation
